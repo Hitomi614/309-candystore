60491e44c9941f0fe8b8083a153aff7fd95c2bdd
ehsueh <ehsueh1993@gmail.com>
2014-03-22 15:32:09 -0400

    added style

--- application/views/product/read.php
+++ application/views/product/read.php
@@ -10,10 +10,11 @@ body{
 	echo "<p>" . anchor('candystore/index','Back') . "</p>";
 	echo "</div>";
 	
+	echo "<div id='content'>";
 	echo "<p> ID = " . $product->id . "</p>";
 	echo "<p> NAME = " . $product->name . "</p>";
 	echo "<p> Description = " . $product->description . "</p>";
 	echo "<p> Price = " . $product->price . "</p>";
 	echo "<p><img src='" . base_url() . "images/product/" . $product->photo_url . "' width='100px'/></p>";
-		
+	echo "</div>";
 ?>	
